ca5e9dec81890d983ca0fd2e19d2ba2f3d054c56
kaiyu li <krisfk@gmail.com>
2021-09-12 00:04:00 +0800

    .

--- wp-content/themes/lfr/index.php
+++ wp-content/themes/lfr/index.php
@@ -275,9 +275,6 @@ wp_reset_postdata();
     </div>
     <div class="row  expertise-row mb-4">
 
-        <div class="col-7">
-            <img class="w-100" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x-50.jpg" alt="">
-        </div>
         <div class="col-5">
 
             <div class="expertise-title">Investment Banking</div>
@@ -291,6 +288,11 @@ wp_reset_postdata();
                 <li>Private Equity</li>
             </ul>
         </div>
+
+        <div class="col-7">
+            <img class="w-100" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x-50.jpg" alt="">
+        </div>
+
     </div>
 
 
