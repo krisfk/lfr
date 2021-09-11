90eeab53b27ab87c5e73132061172adf38e719c2
kaiyu li <krisfk@gmail.com>
2021-09-12 03:46:22 +0800

    .

--- wp-content/themes/lfr/footer.php
+++ wp-content/themes/lfr/footer.php
@@ -21,6 +21,32 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
 
 
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-6">
+                LICENCE NO. 440245 <br>
+                &copy; <?php echo date('Y');?> LFR Hong Kong Limited. All rights reserved.
+
+            </div>
+            <div class="col-6 text-end">
+
+                <ul class="sm-ul">
+                    <li> <a href="#"><img src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-1@2x-1.png"
+                                alt=""></a></li>
+                    <li> <a href="#"><img src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-2@2x.png"
+                                alt=""></a></li>
+                    <li> <a href="#"><img src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x-1.png"
+                                alt=""></a></li>
+                    <li> <a href="#"><img src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-4@2x-1.png"
+                                alt=""></a></li>
+                    <li> <a href="#"><img src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-5@2x-1.png"
+                                alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 </footer><!-- #colophon -->
 
