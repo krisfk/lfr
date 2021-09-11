cd3153edee108bad4e50786bd7d64b7a907f9d95
kaiyu li <krisfk@gmail.com>
2021-09-12 04:27:29 +0800

    .

--- wp-content/themes/lfr/header.php
+++ wp-content/themes/lfr/header.php
@@ -137,5 +137,97 @@ wp_reset_postdata();
                     </div>
 
                     <div>
-                        fds
+                        <div class="container">
+
+
+                            <div class="top-div mt-4 mb-4">
+
+
+                                <div class="row align-items-center">
+
+                                    <div class="col-4">
+
+                                        <a class="logo-a" href="#"><img
+                                                src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-1@2x.png"
+                                                alt=""></a>
+                                    </div>
+
+                                    <div class="col-8 text-end">
+                                        <ul class="top-menu-ul">
+                                            <?php
+$main_menu = wp_get_menu_array('main menu');
+foreach ($main_menu as $menu_item) {
+// echo 1;
+
+$url = $menu_item['url'];
+$title = $menu_item['title'];
+$class = $menu_item['class'];
+
+$temp_arr=explode(get_site_url(),$url);
+$slug=str_replace('/en/','',$temp_arr[1]);
+$slug=str_replace('/cn/','',$slug);
+$slug=str_replace('/','',$slug);
+
+
+if(count($menu_item['children']))
+{
+
+echo '<li><a class="level-1 parent '.$class.'" href="'.$url.'">'.$title.'</a>';
+
+
+echo '<ul class="mobile-menu-submenu">';
+?>
+
+                                            <?php
+
+foreach ($menu_item['children'] as $sub_menu_item) 
+{
+$sub_url = $sub_menu_item['url'];
+$sub_title = $sub_menu_item['title'];
+
+$sub_temp_arr=explode(get_site_url(),$sub_url);
+$sub_slug=str_replace('/en/','',$sub_temp_arr[1]);
+$sub_slug=str_replace('/cn/','',$sub_slug);
+$sub_slug=str_replace('/','',$sub_slug);
+echo'<li><a class="'.$sub_slug.'" href="'.$sub_url.'">'.$sub_title.'</a></li>';
+}
+echo '</ul>';
+
+}
+else
+{
+echo '<li><a class="level-1 '.$slug.' '.$class.'" href="'.$url.'">'.$title.'</a>';
+
+}
+echo'</li>';
+
+
+}
+
+
+
+?>
+
+
+
+
+
+
+
+                                            <?php
+
+wp_reset_postdata(); 
+?>
+
+
+                                        </ul>
+
+                                        </ul>
+
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
