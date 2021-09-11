e6606e74f3e52e1b801a3bdbda1652d75e9280d8
kaiyu li <krisfk@gmail.com>
2021-09-12 00:04:22 +0800

    .

--- wp-content/themes/lfr/index.php
+++ wp-content/themes/lfr/index.php
@@ -290,7 +290,7 @@ wp_reset_postdata();
         </div>
 
         <div class="col-7">
-            <img class="w-100" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x-50.jpg" alt="">
+            <img class="w-100" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-4@2x-50.jpg" alt="">
         </div>
 
     </div>
