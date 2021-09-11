d8264d6d4ff3f06cc82e3218ba888c17d73ad6bb
kaiyu li <krisfk@gmail.com>
2021-09-12 05:09:57 +0800

    .

--- wp-content/themes/lfr/header.php
+++ wp-content/themes/lfr/header.php
@@ -29,6 +29,7 @@
     </script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
--- wp-content/themes/lfr/index.php
+++ wp-content/themes/lfr/index.php
@@ -391,68 +391,6 @@ get_header();
 </div>
 <script type="text/javascript">
 $(function() {
-
-    $('.top-menu-ul a').click(function(e) {
-        // e.preventDefault(0);
-        $('.top-menu-ul a').removeClass('active');
-        $(this).addClass('active');
-    });
-
-
-    $(window).scroll(function() {
-
-        var nowPos = $(document).scrollTop();
-        var nh = $('.fixed-navigation').height();
-        // console.log(nowPos)
-        if (nowPos < $('#services').offset().top - nh) {
-            console.log('home')
-
-        }
-
-
-        if (nowPos > $('#services').offset().top - nh && nowPos < $('#client-process').offset().top -
-            nh) {
-            console.log('services')
-        }
-
-        if (nowPos > $('#client-process').offset().top - nh && nowPos < $('#expertise').offset()
-            .top -
-            nh) {
-            console.log('client-process')
-        }
-
-        if (nowPos > $('#expertise').offset().top - nh && nowPos < $('#the-team').offset()
-            .top -
-            nh) {
-            console.log('expertise')
-        }
-
-        if (nowPos > $('#the-team').offset().top - nh && nowPos < $('#contact-us').offset()
-            .top -
-            nh) {
-            console.log('the-team')
-        }
-
-
-        if (nowPos > $('#contact-us').offset().top - nh - ($(window).height() - $('#contact-us')
-                .height() - nh - $('footer').height())) {
-            console.log('contact-us');
-        }
-
-
-
-
-
-        // if (nowPos > $('#service').offset().top && nowPos < $('#client-process').offset().top) {
-        //     console.log('service');
-
-        // }
-
-
-    })
-
-
-})
 </script>
 <?php
 
