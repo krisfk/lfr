acd240f5f5a62de10d649c807296401e558f8561
kaiyu li <krisfk@gmail.com>
2021-09-11 23:23:40 +0800

    .

--- wp-content/themes/lfr/index.php
+++ wp-content/themes/lfr/index.php
@@ -128,7 +128,7 @@ wp_reset_postdata();
         <div class="col-5">
 
             <div class="row align-items-center service-content-row">
-                <div class="col-4"><img class="w-100"
+                <div class="col-4 icon-col"><img class="w-100"
                         src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x.png" alt="">
                 </div>
                 <div class="col-8 service-content-txt">Identifying and closing on the most qualified talent based upon
