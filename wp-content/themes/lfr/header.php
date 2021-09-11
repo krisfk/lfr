023251043ec3c2020ea241c51ce8f3de06c1c0a7
kaiyu li <krisfk@gmail.com>
2021-09-12 04:45:28 +0800

    .

--- wp-content/themes/lfr/header.php
+++ wp-content/themes/lfr/header.php
@@ -27,7 +27,7 @@
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
         integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous">
     </script> -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 
