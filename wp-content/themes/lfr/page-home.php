c46120dd3f5630549a2ad44e826a270177da122e
kaiyu li <krisfk@gmail.com>
2021-09-13 04:57:28 +0800

    .

--- wp-content/themes/lfr/page-home.php
+++ wp-content/themes/lfr/page-home.php
@@ -69,153 +69,13 @@ if( have_rows('services') )
     }
 }
         ?>
-        f
-        <!-- <div class="service-table-wrapper col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-            <table class="service-table ">
-                <tr>
-                    <td class="icon-td p-3">
-                        <img class="w-75" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x.png" alt="">
-                        <div class="service-name">Executive Search</div>
-
-                    </td>
-                    <td class="content-td">Identifying and closing on the most qualified talent
-                        based
-                        upon
-                        an
-                        agreed ideal
-                        candidate specification</td>
-                </tr>
-            </table>
-        </div>
 
 
-        <div class="service-table-wrapper col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-            <table class="service-table ">
-                <tr>
-                    <td class="icon-td p-3">
-                        <img class="w-75" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-4@2x.png" alt="">
-                        <div class="service-name">Mapping</div>
-
-                    </td>
-                    <td class="content-td">Comprehensive and accurate analysis of the industry professionals in a
-                        particular product or region based on client parameters
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <div class="service-table-wrapper col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-            <table class="service-table ">
-                <tr>
-                    <td class="icon-td p-3">
-                        <img class="w-75" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-5@2x.png" alt="">
-                        <div class="service-name">Benchmarking
-                        </div>
-
-                    </td>
-                    <td class="content-td">Advising on appropriate compensation levels for the purposes of attracting
-                        talent, including annual bonus awards, international relocation and retention incentives
-
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <div class="service-table-wrapper col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-            <table class="service-table ">
-                <tr>
-                    <td class="icon-td p-3">
-                        <img class="w-75" src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-6@2x.png" alt="">
-                        <div class="service-name">Competitor Analysis
-
-                        </div>
-
-                    </td>
-                    <td class="content-td">Visibility on resourcing and headcount levels of peer businesses or products
-                        including their respective strategies, market share and performance
-
-                    </td>
-                </tr>
-            </table>
-        </div> -->
 
     </div>
 
-    <!-- 
-    <div class="row justify-content-center">
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-
-            <div class="service-div">
-                <div class="row align-items-center service-content-row g-0">
-                    <div class="col-5 icon-col p-3"><img class="w-75"
-                            src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-3@2x.png" alt="">
-                        <div class="service-name">Executive Search</div>
-
-                    </div>
-                    <div class="col-7 service-content-txt px-4">Identifying and closing on the most qualified talent
-                        based
-                        upon
-                        an
-                        agreed ideal
-                        candidate specification
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-
-            <div class="service-div">
-                <div class="row align-items-center service-content-row g-0">
-                    <div class="col-5 icon-col p-3"><img class="w-75"
-                            src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-4@2x.png" alt="">
-                        <div class="service-name">Mapping</div>
-
-                    </div>
-                    <div class="col-7 service-content-txt px-4">
-                        Comprehensive and accurate analysis of the industry professionals in a particular product or
-                        region based on client parameters
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-
-            <div class="service-div">
-                <div class="row align-items-center service-content-row g-0">
-                    <div class="col-5 icon-col p-3"><img class="w-75"
-                            src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-5@2x.png" alt="">
-                        <div class="service-name">Benchmarking</div>
-
-                    </div>
-                    <div class="col-7 service-content-txt px-4">
-                        Advising on appropriate compensation levels for the purposes of attracting talent, including
-                        annual bonus awards, international relocation and retention incentives
-                    </div>
-                </div>
-            </div>
-        </div>
 
 
-        <div class="col-lg-6 col-md-12 col-sm-12 col-12  mb-4">
-
-            <div class="service-div">
-                <div class="row align-items-center service-content-row g-0">
-                    <div class="col-5 icon-col p-3"><img class="w-75"
-                            src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-6@2x.png" alt="">
-                        <div class="service-name">Competitor Analysis</div>
-
-                    </div>
-                    <div class="col-7 service-content-txt px-4">
-                        Visibility on resourcing and headcount levels of peer businesses or products including their
-                        respective strategies, market share and performance
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-    </div> -->
 </div>
 
 <div class="position-relative blue-bg pt-5 pb-5 mt-5" id="client-process">
@@ -224,25 +84,8 @@ if( have_rows('services') )
     <div class="big-title  text-center">CLIENT PROCESS</div>
 
     <div class="row justify-content-center   mt-4 g-0">
-        <div class="col-lg-6 col-md-11 col-sm-11 col-11 ">Our ability to deliver the best qualified candidates is driven
-            by our experience and
-            creativity combined
-            with
-            meticulous preparation before we begin each mandate. <br> <br>
-
-            The foundations of a successful search are prepared well before we begin an assignment. We constantly
-            develop
-            our understanding of a client's business through regular discussions, meetings and our own ongoing research.
-            When we begin an assignment, we will already have an appreciation of key issues such as strategy, market
-            positioning, client culture, areas of competitive strength and opportunities for profitable growth we also
-            asses’ threats from fundamental shifts in the market place, and we always work to a consistent execution
-            process.<br><br>
-
-            At the core of our search process is a fully integrated database with the details of over 100,000 key and
-            relevant professionals.<br>
-            In addition, for each mandate we also utilise a number of other candidate sources including real-time ‘on
-            the
-            ground' discussions and intelligence gathering.
+        <div class="col-lg-6 col-md-11 col-sm-11 col-11 ">
+            <?php echo get_field('client_process');?>
         </div>
     </div>
     <!-- <img src="http://64.225.99.121/wp-content/uploads/2021/09/Asset-10@2x-50-scaled.jpg" alt=""> -->
