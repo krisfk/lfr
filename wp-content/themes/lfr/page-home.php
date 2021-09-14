496579f062b00b6219df93f71d439aafc02ac5e9
kaiyu li <krisfk@gmail.com>
2021-09-15 02:22:31 +0800

    .

--- wp-content/themes/lfr/page-home.php
+++ wp-content/themes/lfr/page-home.php
@@ -215,19 +215,19 @@ if( have_rows('teammates') )
             <div class="col-lg-5 col-md-5 col-sm-12 col-12  mb-4">
                 <label for="full-name">Full Name</label>
                 <input id="full-name" type="text" class="form-control" placeholder="Enter your full name here">
-                <div class="form-error name-error text-danger mt-2">Please enter your full name.</div>
+                <div class="form-error full-name-error text-danger mt-2">Please enter your full name.</div>
             </div>
             <div class="col-lg-5 col-md-5 col-sm-12 col-12  mb-4">
 
                 <label for="location">Location</label>
                 <input id="location" type="text" class="form-control" placeholder="Enter your location here">
-                <div class="form-error name-error text-danger mt-2">Please enter your location.</div>
+                <div class="form-error location-error text-danger mt-2">Please enter your location.</div>
 
             </div>
             <div class="col-lg-5 col-md-5 col-sm-12 col-12  mb-4">
                 <label for="email-address">Email Address</label>
                 <input id="email-address" type="text" class="form-control" placeholder="Enter your email address here">
-                <div class="form-error name-error text-danger mt-2">Please enter your email address.</div>
+                <div class="form-error email-error text-danger mt-2">Please enter your email address.</div>
 
             </div>
             <div class="col-lg-5 col-md-5 col-sm-12 col-12  mb-4">
@@ -235,14 +235,14 @@ if( have_rows('teammates') )
 
                 <label for="company">Company</label>
                 <input id="company" type="text" class="form-control" placeholder="Enter your companys name here">
-                <div class="form-error name-error text-danger mt-2">Please enter your company name.</div>
+                <div class="form-error company-error text-danger mt-2">Please enter your company name.</div>
 
             </div>
 
             <div class="col-lg-5 col-md-5 col-sm-12 col-12  mb-4">
                 <label for="phone-number">Phone Number</label>
                 <input id="phone-number" type="text" class="form-control" placeholder="Enter your phone number here">
-                <div class="form-error name-error text-danger mt-2">The phone number input is not correct.</div>
+                <div class="form-error phone-number-error text-danger mt-2">The phone number input is not correct.</div>
 
 
 
@@ -251,7 +251,7 @@ if( have_rows('teammates') )
             <div class="col-lg-5 col-md-5 col-sm-12 col-12  mb-4">
                 <label for="industry">Industry</label>
                 <input id="industry" type="text" class="form-control" placeholder="Enter your companys industry here">
-                <div class="form-error name-error text-danger mt-2">Please enter your companys industry.</div>
+                <div class="form-error industry-error text-danger mt-2">Please enter your companys industry.</div>
 
             </div>
 
@@ -262,6 +262,13 @@ if( have_rows('teammates') )
         </div>
     </div>
 </div>
+<script type="text/javascript">
+$(function() {
+
+    $('.form-error').fadeOut(0);
+
+})
+</script>
 
 <?php
 
